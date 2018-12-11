1a72e325b4bf63b20492298f60ea7ec6bbf7605c
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2018-12-11 18:20:11 +0100

    Basic importing facility

--- importer.py
+++ importer.py
@@ -8,11 +8,17 @@ The supported formats are:
 * XML
 '''
 
+import logging
+logger=logging.getLogger(__name__)
+
 from xml.etree.ElementTree import XML,parse
 from xml.etree.ElementInclude import default_loader
+from qgraf_parser.diagram_elements import Diagram
+
 
 def generate_XML_diagrams_node(file_path):
-    """
+    """Generate an abstract representation of a XML <diagrams>
+
     Parameters
     ----------
     file_path : str
@@ -25,3 +31,18 @@ def generate_XML_diagrams_node(file_path):
     """
     return XML(default_loader(file_path, parse)).find("diagrams")
 
+def create_diagrams_from_XML(file_path):
+    """Generate a list of Diagrams objects from XML file
+
+    Parameters
+    ----------
+    file_path : str
+        string path to the XML QGRAF output
+    Returns
+    -------
+    list of qgraf_parser.diagram_elements.Diagram:
+        list of diagram objects in the XML file
+    """
+
+    diagrams_node = generate_XML_diagrams_node(file_path)
+    return [Diagram(diag) for diag in diagrams_node.findall("diagram")]
